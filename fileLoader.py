f1dfdc44ff88687641ce8473fc10e80c82e35256
fujatsu <103193158+fujatsu@users.noreply.github.com>
2023-09-22 13:33:02 +0200

    Updated fileLoader

--- fileLoader.py
+++ fileLoader.py
@@ -4,36 +4,42 @@ import pandas as pd
 
 def loadFile(file_name, path=None):
     if path == None:
-        print(os.path.abspath(os.path.dirname(__file__)))
+        print(f"Path of current program:\n", os.path.abspath(os.path.dirname(__file__)))
         datafolder_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'dataset/'))
     else:
         datafolder_path = path
-    print(datafolder_path)
-    # list files in folder
-    # check if folder exists if not error
-    if not (os.path.isdir(datafolder_path)):
+    
+    # check if folder exists if not then error
+    if (os.path.isdir(datafolder_path)):
+        print(f"Path of dataset folder:\n", datafolder_path)
+    else:
         print("Data folder path does not exist")
         sys.exit()
-    files_list = os.listdir(datafolder_path)
-    for i in files_list:
-        print(i)
     
     file_path = os.path.join(datafolder_path, file_name)
 
-    # assigns data in file, if the file name exists
+    # assign data in file
     if (os.path.isfile(file_path)):
         file_data = pd.read_csv(file_path)
-        print("File succesfully loaded")
-        print(file_data.keys())
     else:
         print("File name does not exist. Remember to include file type in file name")
         sys.exit()
 
+    print("\n*** File succesfully loaded ***")
+    # print("Files in dataset folder:")
+    files_list = os.listdir(datafolder_path)
+    for i in files_list:
+        print(i)
+    # print("\nInfo axis of file:")
+    # print(file_data.keys())
+    print("\nFile preview:")
+    print(file_data.head())
+
     return file_data
 
-# station02 = loadFile("station02.csv", r'C:\Users\andre\OneDrive - Aalborg Universitet\_Universitet\ES7\_ES7 project\literatureAndDataset\dataset')
-
-# print(station02.keys())
+def fileInfo(file):
+    time_start = file[1]
+    print(time_start)
 
 
 def sliceData(name,start_time,end_time):
--- simplePlotter.py
+++ simplePlotter.py
@@ -1,28 +1,28 @@
 #Simple example of how to read and plot data from the PVO dataset
 # Import libraries
 import matplotlib.pyplot as plt
-# Import file handler functions
+# Import functions
 import fileLoader
-# Import plotting functions
 import plotFunctions as pf
 
 #Start by listing data files in PVO dataset
-
+station02 = fileLoader.loadFile("station02.csv")
+fileLoader.fileInfo(station02)
 
 # EXAMPLE Andreas PC
-try:
-    station02 = fileLoader.loadFile("station02.csv", r'C:\Users\andre\OneDrive - Aalborg Universitet\_Universitet\ES7\_ES7 project\literatureAndDataset\dataset')
-except:
-    pass
-# EXAMPLE Jeppe PC
-try:
-    station02 = fileLoader.loadFile("station02.csv", r'C:\Users\jeppe\gits\PV-PowerPrediction\dataset')
-except:
-    pass
+# try:
+#     station02 = fileLoader.loadFile("station02.csv", r'C:\Users\andre\OneDrive - Aalborg Universitet\_Universitet\ES7\_ES7 project\literatureAndDataset\dataset')
+# except:
+#     pass
+# # EXAMPLE Jeppe PC
+# try:
+#     station02 = fileLoader.loadFile("station02.csv", r'C:\Users\jeppe\gits\PV-PowerPrediction\dataset')
+# except:
+#     pass
 
 
 
-print(station02.head())
+# print(station02.head())
 station02_sliced= fileLoader.sliceData(station02,"2018-07-22 16:00:00","2018-09-22 16:00:00")
 # Create a figure to plot on, 2 axes in 1 column
 [fig,ax]=plt.subplots(3,1)
@@ -30,5 +30,4 @@ station02_sliced= fileLoader.sliceData(station02,"2018-07-22 16:00:00","2018-09-
 pf.plotTimeSeries(ax[0],station02_sliced,"power","power")
 pf.plotColumnScatter(ax[1],station02,"power","lmd_windspeed","power vs windspeed")
 pf.plotHistogram(ax[2],station02,"power","power")
-plt.show()  
-
+plt.show()
