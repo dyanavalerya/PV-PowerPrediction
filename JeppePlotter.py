ff41ee1f1a1596c0df944d74ba7051ee8b7a6a82
Jeppe Hansen <jeppe.hansen@agcocorp.com>
2023-10-12 09:52:22 +0200

    JeppeStuff

--- JeppePlotter.py
+++ JeppePlotter.py
@@ -9,12 +9,22 @@ station02_sliced= fileLoader.sliceData(station02,"2018-07-23 16:00:00","2018-07-
 
 fig = plt.figure()
 ax3 = fig.add_subplot(1, 1, 1)
-pf.plotColumnScatter(ax3,station02,"power","nwp_globalirrad","Power vs Global Irradiance")
+plt.title("Scatter plot of Power vs NWP Pressure") 
 
+pf.plotColumnScatter(ax3,station02,"power","nwp_pressure","Power vs NWP Pressure")
 
 fig = plt.figure()
 ax4 = fig.add_subplot(1, 1, 1)
 #pf.plotTimeSeries(ax4,station02_sliced,"nwp_globalirrad","Plot of Global Irradiance",12)
-pf.plotColumnScatter2Y(ax4,station02_sliced,"date_time","power","nwp_globalirrad","Time plot of Power vs Global Irradiance",12)
+pf.plotColumnScatter2Y(ax4,station02_sliced,"date_time","power","nwp_pressure","Time plot of Power vs Global Irradiance",12)
+plt.title("Time plot of Power vs Global Irradiance")
+plt.tight_layout()
+
+
+
+fig = plt.figure()
+ax2 = fig.add_subplot(1, 1, 1)
+pf.correlationMatrixPlotter(ax2,station02)
+ax2.set_title("Correlation matrix of station02 dataset")
 plt.tight_layout()
 plt.show()
